ef07565531e4aac1d8a68358182d720c7c630ba9
Shinichi Maeshima <netwillnet@gmail.com>
2024-07-28 17:48:49 +0900

    initial commit

---
+++ lib/save_chain_inspector.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "save_chain_inspector/version"
+
+module SaveChainInspector
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/save_chain_inspector_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe SaveChainInspector do
+  it "has a version number" do
+    expect(SaveChainInspector::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
